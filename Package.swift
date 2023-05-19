507e77de01ce3a4ea1364993c4b12144f56ba9e9
Kraig Spear <kraigspear@gmail.com>
2023-05-19 06:01:36 -0400

    Updating SpearFoundation Dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/kraigspear/Spearfoundation.git", branch: "iOS16")
+        .package(url: "https://github.com/kraigspear/Spearfoundation.git", .exact("2.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
